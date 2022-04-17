bd101428485f888de164b499f633cc27f4072a0a
Maddie-Gr <mgrabosk@ucsc.edu>
2022-04-17 14:45:22 -0700

    fecqv

---
+++ lab4/js/lab.js
@@ -0,0 +1,28 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+//How to make and eat cereal
+//Gather supplies
+  //bowl
+  //cereal
+  //milk
+  //spoon
+//Pour desired amount of cereal into bowl
+//Pour desired amount of milk into the same bowl
+//Put spoon in bowl
+//Collect cereal and milk on the spoon
+//Put the contents of the spoon into your mouth
+//Repeat steps 4-6 until bowl is empty
+
+//How to make a calculator
+//Display numbers and symbols
+//Accept user input of equation
+//Validate user equation
+//Locate appropriate operations for given equation
+//Calculate result of user equation
+//Validate result of equation
+//Print result
