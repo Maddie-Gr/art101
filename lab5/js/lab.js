9805fbe772823e57f39abc566ea1fe3581e366c0
Maddie-Gr <mgrabosk@ucsc.edu>
2022-04-20 11:14:12 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+ * Author: Madeleine Graboske
+ * Created: 04 / 19 / 2022
+ *
+ */
+
+
+//Define Variables
+  make = "Tesla";
+  model = "S";
+  color = "Black";
+  year = 2012;
+
+//Calculate
+  age = 2022 - year;
+
+//Output
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + "<br>");
