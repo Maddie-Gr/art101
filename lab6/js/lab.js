8d849866a082c2e11fe9b365a37e0787b33f9120
Maddie-Gr <mgrabosk@ucsc.edu>
2022-04-25 10:50:15 -0700

    lab 6

---
+++ lab6/js/lab.js
@@ -0,0 +1,24 @@
+/*
+ * Author: Madeleine Graboske
+ * Created: 04 / 22 / 2022
+ *
+ */
+
+ //Define Variables
+
+ myTransport = ["Prius", "plane", "rides from friends"];
+
+ myMainRide = {
+   make : "Prius",
+   model : "Prime",
+   color : "White",
+   year : 2017,
+   age : function() {
+     return 2022 - year;
+   }
+ }
+
+ //output
+ document.writeln("Transportation I use: ", myTransport, "</br");
+ document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
