2bb9604664a6763eb08cf98c04f18060e045807f
Maddie-Gr <mgrabosk@ucsc.edu>
2022-04-26 08:32:37 -0700

    lab 7

---
+++ lab7/js/lab.js
@@ -0,0 +1,24 @@
+/*
+ * Author: Madeleine Graboske
+ * Created: 04 / 26 / 2022
+ *
+ */
+
+function myfunc(){
+  var userName = window.prompt("Hello! Please tell me your name! (You can trust me :D)");
+  console.log("userName =", userName);
+  //split time
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  //sort
+  var sortnameArray = nameArray.sort();
+  console.log("sortnameArray =", sortnameArray);
+  //join
+  var nameSorted = sortnameArray.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+
+}
+//output
+document.writeln("I have taken your name, it is mine now. Here is a new one: ",
+  myfunc(), "</br>");
