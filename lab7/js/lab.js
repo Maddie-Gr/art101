a7f84da67cf309abab3618d68839ac5dcb81af61
Maddie-Gr <mgrabosk@ucsc.edu>
2022-04-27 11:14:04 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -6,19 +6,21 @@
 
 function myfunc(){
   var userName = window.prompt("Hello! Please tell me your name! (You can trust me :D)");
-  console.log("userName =", userName);
-  //split time
-  var nameArray = userName.split('');
-  console.log("nameArray =", nameArray);
-  //sort
-  var sortnameArray = nameArray.sort();
-  console.log("sortnameArray =", sortnameArray);
-  //join
-  var nameSorted = sortnameArray.join('');
-  console.log("nameSorted =", nameSorted);
-  return nameSorted;
+
+
+    console.log("userName =", userName);
+    //split time
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    //sort
+    var sortnameArray = nameArray.sort();
+    console.log("sortnameArray =", sortnameArray);
+    //join
+    var nameSorted = sortnameArray.join('');
+    console.log("nameSorted =", nameSorted);
+    return ("Darth " + nameSorted);
 
 }
 //output
-document.writeln("I have taken your name, it is mine now. Here is a new one: ",
+document.writeln("I have taken your name, it is mine now. You shall now be known as, ",
   myfunc(), "</br>");
