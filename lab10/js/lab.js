85b9c16fab90692429aa09a427482afdc301175d
Maddie-Gr <mgrabosk@ucsc.edu>
2022-05-08 21:02:11 -0700

    lab10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -5,23 +5,19 @@
  */
 
 //make it so prompt is on page and not a popup
- function myfunc(){
-   var userName = window.prompt("Hello! Please tell me your name! (You can trust me :D)");
 
+//Variables
+  var button = document.getElementById("button");
+  var output = document.getElementById("output");
 
-     console.log("userName =", userName);
-     //split time
-     var nameArray = userName.split('');
-     console.log("nameArray =", nameArray);
-     //sort
-     var sortnameArray = nameArray.sort();
-     console.log("sortnameArray =", sortnameArray);
-     //join
-     var nameSorted = sortnameArray.join('');
-     console.log("nameSorted =", nameSorted);
-     return (nameSorted);
-
- }
- //output
- document.writeln("I have taken your name, it is mine now. You shall now be known as, ",
-   myfunc(), "</br>");
+//button listener
+  button.addEventListener("click", function(){
+    var name = document.getElementById("name").value;
+    //split
+    var nameArray = name.split('');
+    //sort
+    var nameArraySort = nameArray.sort();
+    //join
+    var nameSorted = nameArraySort.join('');
+    output.innerHTML = nameSorted;
+  });
