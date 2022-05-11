ba338d11c118b6b7ef23fddefd84a72d57274ef1
Maddie-Gr <mgrabosk@ucsc.edu>
2022-05-11 10:34:09 -0700

    lab11 stuff

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -3,4 +3,24 @@
  * Created: 05 / 09 / 2022
  *
  */
- 
+
+//create button and add to section
+
+$("#challenge").append("<button id='chal'>Button</button>");
+$("#problem").append("<button id='prob'>Button</button>");
+$("#results").append("<button id='res'>Button</button>");
+
+$("#chal").click(function(){
+  // find the section, toggle a class
+  // $("#challenge").toggleClass("special");
+
+  // here's a different maybe better way to do it
+  // (we are traversing the DOM to the parent)
+  $(this).parent().toggleClass("new");
+})
+
+$("#res").click(function(){
+  // here's a different maybe better way to do it
+  // (we are traversing the DOM to the parent)
+  $(this).parent().toggleClass("new2");
+})
