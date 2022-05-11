acf3f855cdf83af6995f8118564f573c0ef2a265
Maddie-Gr <mgrabosk@ucsc.edu>
2022-05-11 11:58:06 -0700

    la11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -5,17 +5,17 @@
  */
 
 //create button and add to section
-
-$("#challenge").append("<button id='chal'>Button</button>");
 $("#problem").append("<button id='prob'>Button</button>");
+$("#challenge").append("<button id='chal'>Button</button>");
 $("#results").append("<button id='res'>Button</button>");
+
+$("#prob").click(function(){
+  // find the section, toggle a class
+  $(this).parent().toggleClass("new3");
+})
 
 $("#chal").click(function(){
   // find the section, toggle a class
-  // $("#challenge").toggleClass("special");
-
-  // here's a different maybe better way to do it
-  // (we are traversing the DOM to the parent)
   $(this).parent().toggleClass("new");
 })
 
