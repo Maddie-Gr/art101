d52b36b59c67e57c10573929eaad809aa32f53bf
Maddie-Gr <mgrabosk@ucsc.edu>
2022-05-26 08:46:58 -0700

    Update lab.js

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -5,7 +5,20 @@
  */
 
 
-
+ {
+ "month": "5",
+ "num": 2624,
+ "link": "",
+ "year": "2022",
+ "news": "",
+ "safe_title": "Voyager Wires",
+ "transcript": "",
+ "alt": "Also, they're getting increasingly worried that someone will accidentally hit the 'retract' button, and that the end of the cable thrashing around as it winds up could devastate the Earth's surface.",
+ "img": "https://imgs.xkcd.com/comics/voyager_wires.png",
+ "title": "Voyager Wires",
+ "day": "25"
+ }
+ 
  // Getting the URL
  var endpoint = "https://xkcd.com/info.0.json";
 
@@ -25,18 +38,3 @@
 
  // Creating the button
  $("button").click(getAjax);
-
-
-{
-"month": "5",
-"num": 2624,
-"link": "",
-"year": "2022",
-"news": "",
-"safe_title": "Voyager Wires",
-"transcript": "",
-"alt": "Also, they're getting increasingly worried that someone will accidentally hit the 'retract' button, and that the end of the cable thrashing around as it winds up could devastate the Earth's surface.",
-"img": "https://imgs.xkcd.com/comics/voyager_wires.png",
-"title": "Voyager Wires",
-"day": "25"
-}
