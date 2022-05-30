79f505d391e346bab810eceb28cdcd266e5ceee5
Maddie-Gr <mgrabosk@ucsc.edu>
2022-05-30 13:36:02 -0700

    warge

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -5,36 +5,22 @@
  */
 
 
- {
- "month": "5",
- "num": 2624,
- "link": "",
- "year": "2022",
- "news": "",
- "safe_title": "Voyager Wires",
- "transcript": "",
- "alt": "Also, they're getting increasingly worried that someone will accidentally hit the 'retract' button, and that the end of the cable thrashing around as it winds up could devastate the Earth's surface.",
- "img": "https://imgs.xkcd.com/comics/voyager_wires.png",
- "title": "Voyager Wires",
- "day": "25"
- }
- 
- // Getting the URL
- var endpoint = "https://xkcd.com/info.0.json";
 
- //Creating the function
- function getAjax() {
-  $.ajax({
-    url: endpoint,
+ var endpoint = "https://xkcd.com/614/info.0.json";
+
+function getAjax() {
+	$.ajax({
+  	url: endpoint,
     type: "GET"
   })
   .done(function(data){
-    $("#output").html("<h3>" + data.quote + "</h3>")
+  	$("#output").append("<h3>" + data.title + "</h3>");
+  	$("#output").append("<img src=" + data.img + ">" + "<br>");
+  	$("#output").append( data.alt );
   })
   .fail(function(request, error){
-    $("#output").html("Didn't Work.");
+  	$("#output").html("Something fucked up.");
   })
- }
+}
 
- // Creating the button
- $("button").click(getAjax);
+$("button").click(getAjax);
